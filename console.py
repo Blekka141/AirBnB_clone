81cb9f3b2fceeecd1bbe91a7b74eb9495b6f0a01
Keegan <keeganjoubert@protonmail.com>
2024-03-04 10:06:53 -0800

    Updates

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines the HBnB console."""
+import cmd
+import re
+from shlex import split
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import
+
+
+
+"""Last part of console.py"""
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
